4080b8111b02846add74e140231d22326bd8b0f6
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-08-25 19:15:50 -0300

    refact: clear application

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, RenderOptions, RenderResult } from '@testing-library/react'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
@@ -14,8 +14,10 @@ const Providers = ({ children }) => {
   // )
 }
 
-const customRender = (ui, options = {}) =>
-  render(ui, { wrapper: Providers, ...options })
+const customRender = (
+  ui: JSX.Element,
+  options: RenderOptions = {}
+): RenderResult => render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
