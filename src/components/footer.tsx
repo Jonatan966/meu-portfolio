03954708a965799bb34bf5eed60e09f91663cd31
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-09-07 16:30:20 -0300

    feat: footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import { FooterContainer } from '../styles/components/footer'
+
+export function Footer(): JSX.Element {
+  return (
+    <FooterContainer>
+      <p>
+        Feito com <strong>NextJS</strong> e <strong>Styled Components</strong> -
+        © 2021 <strong>Jonatan Frederico</strong>
+      </p>
+    </FooterContainer>
+  )
+}
---
+++ src/styles/components/footer.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.footer`
+  text-align: center;
+
+  padding: 1rem;
+`
