76d12d61962aea85290c8848d8aca8ac206c9d68
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-09-08 23:29:14 -0300

    refact: add id optionally in parse prismic list

--- src/utils/parse-prismic-list.ts
+++ src/utils/parse-prismic-list.ts
@@ -1,12 +1,15 @@
-export function parsePrismicList(list: any[]): any[] {
+export function parsePrismicList(list: any[], withId = true): any[] {
   const cloneList = []
 
   for (const listItemIndex in list) {
     const [, listItemContent] = Object.entries<any>(list[listItemIndex])[0]
 
     cloneList[listItemIndex] = {
-      ...listItemContent.data,
-      id: listItemContent.id,
+      ...(listItemContent?.data || listItemContent),
+    }
+
+    if (withId) {
+      cloneList[listItemIndex].id = listItemContent.id
     }
   }
 
