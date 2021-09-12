f8d3dfa1870ce9faa98b020e9ded15782cd6a81b
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-09-12 15:19:14 -0300

    feat: post page

--- src/styles/components/post-article.ts
+++ src/styles/components/post-article.ts
@@ -3,10 +3,14 @@ import { DefaultSection } from './default-section'
 
 export const PostArticle = styled(DefaultSection)`
   h6 {
-    margin: 1.35rem 0;
+    margin: 0.25rem 0;
     font-weight: normal;
 
     opacity: 0.75;
+  }
+
+  header {
+    margin-bottom: 1.5rem;
   }
 
   p + p {
