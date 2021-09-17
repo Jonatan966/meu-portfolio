ad2dc313a0c01f80334bb1e51dc9ee81e1eb7f33
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-09-16 21:58:17 -0300

    feat: add more responsivity into grid list

--- src/styles/components/grid-list.ts
+++ src/styles/components/grid-list.ts
@@ -10,6 +10,10 @@ export const GridList = styled.div<GridListProps>`
   grid-template-columns: repeat(${(props) => props.columns}, 1fr);
   grid-gap: ${(props) => props.gap}rem;
 
+  @media (max-width: 540px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
   @media (max-width: 425px) {
     grid-template-columns: repeat(1, 1fr);
   }
