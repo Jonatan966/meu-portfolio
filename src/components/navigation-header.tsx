e80a208b20cd0827c851b6d04bdcf5c154e73f77
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-01-21 16:44:56 -0300

    refact: use nextjs image component to render profile image

--- src/components/navigation-header.tsx
+++ src/components/navigation-header.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import { SocialLinks } from "./social-links";
+import Image from "next/image";
 
 export function NavigationHeader() {
   return (
     <header className="border-b-2 border-[#232327] sticky top-0 bg-[#121214] z-30">
       <div className="max-w-[1050px] mx-auto p-4 flex items-center">
-        <img
+        <Image
           src="https://github.com/Jonatan966.png"
           alt="Foto de perfil de Jonatan Frederico"
           className="w-8 h-8 rounded-full mr-2"
+          width={32}
+          height={32}
         />
         <h1 className="font-semibold">Jonatan Frederico</h1>
 
