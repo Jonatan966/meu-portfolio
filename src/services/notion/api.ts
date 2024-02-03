e0f2173fff65aa4b8679b0b0c4c1cb4505b97e14
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-02-03 16:10:17 -0300

    refact: use nextjs revalidate

--- src/services/notion/api.ts
+++ src/services/notion/api.ts
@@ -6,6 +6,6 @@ export const notionApi = new Client({
   fetch: (url, init) =>
     fetch(url, {
       ...init,
-      next: { revalidate: false } /* next: { revalidate: 3600 } */,
+      next: { revalidate: 3600 },
     }),
 });
