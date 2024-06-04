5eb31f18758deff60c860938df6e4cb141f55223
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-06-04 20:52:10 -0300

    feat: add projects total label

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,7 +7,12 @@ export default async function ProjectsPage() {
   return (
     <main>
       <section className="max-w-[1050px] mx-auto p-4 mt-2">
-        <h2 className="text-xl font-medium">Projetos</h2>
+        <div className="flex items-center gap-2">
+          <h2 className="text-xl font-medium">Projetos</h2>
+          <span className="text-xs bg-white text-black rounded-xl px-2 py-1 font-bold">
+            {projects.length} no total
+          </span>
+        </div>
         <ul className="grid grid-cols-1 gap-4 mt-2 md:grid-cols-3">
           {projects.map((project) => (
             <ProjectCard key={project.id} {...{ project }} />
