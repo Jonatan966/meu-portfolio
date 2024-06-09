9902914e9ae3f04582da9468f5dbf01f115a92b7
Jonatan Frederico <jonatanfrederico@gmail.com>
2024-06-09 02:57:11 -0300

    feat: add more styles to markdown component

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -7,11 +7,17 @@ interface MarkdownProps {
 export function Markdown(props: MarkdownProps) {
   return (
     <ReactMarkdown
+      className="flex flex-col gap-4 text-stone-300"
       components={{
         p: (props) => (
-          <p {...props} className="leading-relaxed text-stone-300">
+          <p {...props} className="text-lg leading-relaxed">
             {props.children}
           </p>
+        ),
+        h3: (props) => (
+          <h3 {...props} className="text-lg  font-bold">
+            {props.children}
+          </h3>
         ),
       }}
     >
